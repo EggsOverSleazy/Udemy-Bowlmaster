15ec8b0e54655a2856d08f8bf436bc7445c23a09
itsRichard <richardtlee@gmail.com>
2016-05-15 15:25:45 -0700

    Initial commit
    asdf

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public Vector3 launchVelocity = new Vector3 (0,0,300f);
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.transform.position= new Vector3(1, 20, 30);
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+
+        if (gameObject.GetComponent<Rigidbody>().velocity == new  Vector3(0, 0, 0))
+        {
+            gameObject.GetComponent<AudioSource>().Stop();
+        }
+	
+	}
+
+    private void OnMouseDown()
+    {
+        Launch();
+    }
+
+    private void Launch()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = launchVelocity;
+    }
+
+    private void OnCollisionEnter()
+    {
+        PlaySoundOnCollision();
+    }
+
+    private void PlaySoundOnCollision()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+}
---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    public Ball ball;
+    private Vector3 offset;
+ 
+	// Use this for initialization
+	void Start () {
+        // Capture original offset
+        offset = gameObject.transform.position - ball.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(gameObject.transform.position.z <= 1829f-200f)
+        {
+            gameObject.transform.position = ball.transform.position + offset;
+        }
+	}
+}
