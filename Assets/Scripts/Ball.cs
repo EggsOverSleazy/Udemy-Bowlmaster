997888c8503245abc8271cb9aac117b0a3e14c10
itsRichard <richardtlee@gmail.com>
2016-05-15 21:29:13 -0700

    Add drag and launch system

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,31 +4,47 @@ using System.Collections;
 public class Ball : MonoBehaviour {
 
     public Vector3 launchVelocity = new Vector3 (0,0,300f);
+    public GameObject pins;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+    }
 
     // Update is called once per frame
-    void Update () {
+    void Update ()
+    {
+        Reset();
+        StopSoundOnStill();
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            gameObject.transform.position= new Vector3(1, 20, 30);
-            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
-        }
-
-        if (gameObject.GetComponent<Rigidbody>().velocity == new  Vector3(0, 0, 0))
+    private void StopSoundOnStill()
+    {
+        if (gameObject.GetComponent<Rigidbody>().velocity == new Vector3(0, 0, 0))
         {
             gameObject.GetComponent<AudioSource>().Stop();
         }
-	
-	}
-
-    private void OnMouseDown()
-    {
-        Launch();
     }
 
-    private void Launch()
+    private void Reset()
     {
-        gameObject.GetComponent<Rigidbody>().velocity = launchVelocity;
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            gameObject.transform.position = new Vector3(1, 20, 30);
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+
+            //Destroy(GameObject.Find("Pins"));
+        }
+
+        
+        //Instantiate(pins, new Vector3(0, 0, 1829), Quaternion.identity);
+      
+    }
+
+    public void Launch(Vector3 velocity)
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        gameObject.GetComponent<Rigidbody>().velocity = velocity;
     }
 
     private void OnCollisionEnter()
---
+++ Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//add Ball script if it wasn't already on this component.  
+[RequireComponent (typeof(Ball))] 
+
+public class DragLaunch : MonoBehaviour {
+
+    private Ball ball;
+    private Vector3 dragStartPosition, dragEndPosition;
+    private float dragStartTime, dragEndTime;
+
+    void Start()
+    {
+        ball = GetComponent<Ball>();
+    }
+
+    public void DragStart()
+    {
+        // capture time and position of drag start (mouse click)
+        dragStartPosition = Input.mousePosition;
+        dragStartTime = Time.time;
+    }
+
+    public void DragEnd()
+    {
+        // launch the ball based on the drag 
+        dragEndPosition = Input.mousePosition;
+        dragEndTime = Time.time;
+
+        float dragDuration = dragEndTime - dragStartTime;
+        float launchSpeedX = (dragEndPosition.x - dragStartPosition.x) / dragDuration;
+        float launchSpeedZ = (dragEndPosition.y - dragStartPosition.y) / dragDuration;
+        //Y on the touch interface = Z in the world
+
+        // launch
+        //Vector3 launchVelocity = new Vector3(launchSpeedX, 0, launchSpeedZ);
+        ball.Launch(new Vector3(launchSpeedX, 0, launchSpeedZ));
+
+
+    }
+
+
+}
