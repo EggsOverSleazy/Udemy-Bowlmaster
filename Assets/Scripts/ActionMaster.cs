d27ba6e6833ce9b59371d3bfa53826b4cd6047fc
itsRichard <richardtlee@gmail.com>
2016-05-24 20:05:43 -0700

    Test based development is fucking boringgg

---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionMaster {
+
+    public enum Action
+    {
+        Tidy, Reset, EndTurn, EndGame
+    };
+
+    public Action Bowl(int pins)    // returns something of type acdtion
+    {
+        if (pins < 0 || pins > 10)
+        {
+            throw new UnityException("Durr.. invalid pins!");
+        }
+
+        if (pins == 10)
+        { 
+            return Action.EndTurn;
+        }
+
+        throw new UnityException("Durr.. Not sure what action to return!");
+    }
+}
