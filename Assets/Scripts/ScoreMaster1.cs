7ba11fa0bed8f88376c140e3020a02c7191f35b6
itsRichard <richardtlee@gmail.com>
2016-06-06 22:41:15 -0700

    asdf

---
+++ Assets/Scripts/ScoreMaster1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;   //needed in order to use lists  
+
+public class ScoreMastery {
+
+    public static List<int> ScoreFrames (List<int> rolls)
+    {
+        List<int> frameList = new List<int>();  // curly brackets because its a constructor
+
+        // your code here
+
+        return frameList;
+    }
+
+}
